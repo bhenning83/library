09a252a449a39be23ad4ccc4bd9a22d3511bfda8
Brendon Henning <brendonhenning@gmail.com>
2020-11-05 11:20:26 -0600

    added delete button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 let myLibrary = [];
-let cards = document.querySelector(".cards-container");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -14,24 +13,65 @@ function Book(title, author, pages, read) {
   }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  let book = new Book(title, author, pages, read);
+function addBookToLibrary() {
+  let book = new Book(
+    form.title.value,
+    form.author.value,
+    form.pages.value,
+    form.read.checked
+    );
+
   myLibrary.push(book);
 }
 
 function showLibrary() {
-  myLibrary.forEach(book => {
-    let card = document.createElement('div');
+  let cards = document.querySelector(".cards-box");
+  cards.innerHTML = '';
+  myLibrary.forEach((book, i) => {
+    let del = makeDelButton();
+    const card = document.createElement('div');
     card.classList.add('card');
+    card.id = i;
     card.innerText = `Title: ${book.title}
                       Author: ${book.author}
                       Number of pages: ${book.pages}
                       Already read: ${book.read}
                       \n`;
     cards.appendChild(card);
+    card.appendChild(del);
   });
 }
-addBookToLibrary('East of Eden', 'John Steinbeck', 420, true);
-addBookToLibrary('The God Delusion', 'Richard Dawkins', 345, true);
-addBookToLibrary('How to Change Your Mind', 'Michael Pollen', 400, true);
+
+function createCard(book, cards, i) {
+  
+}
+
+function makeDelButton() {
+  del = document.createElement('button')
+  del.innerText = 'Delete';
+  del.classList.add('btn');
+  del.classList.add('btn-danger');
+  del.addEventListener('click', removeBook)
+  return del;
+}
+
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  form.reset();
+  showLibrary();
+});
+
+function removeBook(e) {
+  let idx = e.target.parentNode.id
+  myLibrary.splice(idx, 1);
+  showLibrary();
+}
+
+EastofEden = new Book('East of Eden', 'John Steinbeck', 420, true);
+god = new Book('The God Delusion', 'Richard Dawkins', 345, true);
+mind = new Book('How to Change Your Mind', 'Michael Pollen', 400, true);
+lotr = new Book('Lord of the Rings: the Fellowship of the Ring', 'JRR Tolkein', 296, false);
+myLibrary.push(EastofEden, god, mind, lotr);
 showLibrary();
