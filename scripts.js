1a62905e6dcd9ea2b73621fb102671a186acf5a7
Brendon Henning <brendonhenning@gmail.com>
2020-11-05 20:55:50 -0600

    done without storage

--- scripts.js
+++ scripts.js
@@ -5,13 +5,20 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-
-  this.info = function() {
-    let insert = ''
-    read == false ? insert = "You haven't read this." : insert = "You have read this.";
-    console.log(`${name}, by ${author}, ${pages} pages. ${insert}`);
-  }
 }
+
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
+};
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  form.reset();
+  showLibrary();
+});
 
 function addBookToLibrary() {
   let book = new Book(
@@ -29,25 +36,25 @@ function showLibrary() {
   cards.innerHTML = '';
   myLibrary.forEach((book, i) => {
     let del = makeDelButton();
+    let toggle = makeToggleButton();
     const card = document.createElement('div');
     card.classList.add('card');
     card.id = i;
+    let readStatus = '';
+    if (book.read == true) readStatus = 'Already read it';
+    if (book.read == false) readStatus = "Haven't read it yet";
     card.innerText = `Title: ${book.title}
                       Author: ${book.author}
                       Number of pages: ${book.pages}
-                      Already read: ${book.read}
-                      \n`;
+                      ${readStatus}`;
     cards.appendChild(card);
+    card.appendChild(toggle);
     card.appendChild(del);
   });
 }
 
-function createCard(book, cards, i) {
-  
-}
-
 function makeDelButton() {
-  del = document.createElement('button')
+  del = document.createElement('button');
   del.innerText = 'Delete';
   del.classList.add('btn');
   del.classList.add('btn-danger');
@@ -55,19 +62,34 @@ function makeDelButton() {
   return del;
 }
 
-const form = document.querySelector('form')
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  addBookToLibrary();
-  form.reset();
-  showLibrary();
-});
+function makeToggleButton() {
+  tog = document.createElement('button')
+  tog.innerText = 'Change read status';
+  tog.classList.add('btn');
+  tog.classList.add('btn-light');
+  tog.addEventListener('click', changeRead);
+  return tog;
+}
 
 function removeBook(e) {
   let idx = e.target.parentNode.id
   myLibrary.splice(idx, 1);
   showLibrary();
 }
+
+function changeRead(e) {
+  console.log(e.target.parentNode.parentNode.parentNode)
+  myLibrary[e.target.parentNode.id].toggleRead();
+  showLibrary();
+}
+
+card = document.getElementsByClassName('card')
+
+const dbRefObject = firebase.database().ref().child('object');
+
+dbRefObject.on('value', snap => console.log(snap.val()));
+
+
 
 EastofEden = new Book('East of Eden', 'John Steinbeck', 420, true);
 god = new Book('The God Delusion', 'Richard Dawkins', 345, true);
