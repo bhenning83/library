e2a0268f59ba75f8107567e91d490e80f2da1ff2
Brendon Henning <brendonhenning@gmail.com>
2020-11-06 09:11:02 -0600

    configed firebase

--- scripts.js
+++ scripts.js
@@ -35,8 +35,6 @@ function showLibrary() {
   let cards = document.querySelector(".cards-box");
   cards.innerHTML = '';
   myLibrary.forEach((book, i) => {
-    let del = makeDelButton();
-    let toggle = makeToggleButton();
     const card = document.createElement('div');
     card.classList.add('card');
     card.id = i;
@@ -48,7 +46,11 @@ function showLibrary() {
                       Number of pages: ${book.pages}
                       ${readStatus}`;
     cards.appendChild(card);
+
+    let toggle = makeToggleButton();
     card.appendChild(toggle);
+
+    let del = makeDelButton();
     card.appendChild(del);
   });
 }
@@ -83,17 +85,36 @@ function changeRead(e) {
   showLibrary();
 }
 
-card = document.getElementsByClassName('card')
-
-const dbRefObject = firebase.database().ref().child('object');
-
-dbRefObject.on('value', snap => console.log(snap.val()));
-
-
-
 EastofEden = new Book('East of Eden', 'John Steinbeck', 420, true);
 god = new Book('The God Delusion', 'Richard Dawkins', 345, true);
 mind = new Book('How to Change Your Mind', 'Michael Pollen', 400, true);
 lotr = new Book('Lord of the Rings: the Fellowship of the Ring', 'JRR Tolkein', 296, false);
 myLibrary.push(EastofEden, god, mind, lotr);
 showLibrary();
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCDLxoWNQM9IsQC9-WFQy_joqcdBWzWjCk",
+  authDomain: "library-41aba.firebaseapp.com",
+  databaseURL: "https://library-41aba.firebaseio.com",
+  projectId: "library-41aba",
+  storageBucket: "library-41aba.appspot.com",
+  messagingSenderId: "273428719470",
+  appId: "1:273428719470:web:f13d601edf20f78e2d437f",
+  measurementId: "G-R7Z3V222XZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+database = firebase.database();
+
+const dbRefObject = database.ref().child('object');
+
+dbRefObject.on('value', snap => {
+  if (snap.exists() == false) {
+    database.ref().set(myLibrary)
+  } else {
+    myLibrary = snap.val();
+  }
+});
+
+
